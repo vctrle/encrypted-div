00e09c3e1ac8d0a8b73da0572001cea971e126c5
vctrle <63797801+vctrle@users.noreply.github.com>
2024-07-19 15:46:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,25 +7,26 @@ document.getElementById('encryptButton').addEventListener('click', function() {
         const libraryLink = '<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js"><\/script>';
         const decryptionScript = `
 <script>
-	document.getElementById('decryptButton').addEventListener('click', function() {
-		const decryptPasscode = document.getElementById('decryptPasscode').value;
-		if (decryptPasscode) {
-			try {
-				const encryptedContent = document.getElementById('encryptedData').innerText;
-				const decrypted = CryptoJS.AES.decrypt(encryptedContent, decryptPasscode).toString(CryptoJS.enc.Utf8);
-				if (decrypted) {
-					const decryptedDiv = document.getElementById('encryptedContent');
-					decryptedDiv.innerHTML = decrypted;
-				} else {
-					alert('Incorrect passcode');
-				}
-			} catch (e) {
-				alert('Incorrect passcode');
-			}
-		} else {
-			alert('Please enter a passcode');
-		}
-	});
+    document.getElementById('decryptButton').addEventListener('click', function() {
+        const decryptPasscode = document.getElementById('decryptPasscode').value;
+        if (decryptPasscode) {
+            try {
+                const encryptedContent = document.getElementById('encryptedData').innerText;
+                const decrypted = CryptoJS.AES.decrypt(encryptedContent, decryptPasscode).toString(CryptoJS.enc.Utf8);
+                if (decrypted) {
+                    const decryptedDiv = document.getElementById('encryptedContent');
+                    decryptedDiv.innerHTML = decrypted;
+                    decryptedDiv.style.whiteSpace = 'pre'; // Preserve formatting
+                } else {
+                    alert('Incorrect passcode');
+                }
+            } catch (e) {
+                alert('Incorrect passcode');
+            }
+        } else {
+            alert('Please enter a passcode');
+        }
+    });
 <\/script>
         `;
         document.getElementById('htmlInput').value = `
